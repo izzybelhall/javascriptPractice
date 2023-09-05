99980a30a7894055e8b06c28197164cbbcceb470
Isabel Hall <washington.isabel@yahoo.com>
2023-09-05 19:40:44 -0400

    added js

---
+++ script.js
@@ -0,0 +1,11 @@
+/*basic math problem
+x=length,y=width,z=height
+1. Find the area
+2. Find the perimeter
+3. Find the volume
+4. How much room would be left over if you put the object in an area of 100units? */
+let x=7,y=9,z=15
+console.log(x*y)
+console.log((x*2)+(y*2))
+console.log(x*y*z)
+console.log(100-(x*y))
